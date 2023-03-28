7865b090c7eb1134062cb811709d7fba766e972c
BNicDev <bnicdev@gmail.com>
2023-03-28 12:13:23 -0300

    algunos progresos

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,12 @@
+function square(num){
+    return num * num;
+}
+
+//arrow function
+
+const square = (num) => {
+    return num * num;
+}
+// si solo pasamos un solo argumento la funcion la podes realizar asi
+
+const square = num => num * num;
---
+++ src/es6/02-strings.js
@@ -0,0 +1,5 @@
+let hello = "hello";
+let world = "world";
+
+let epicPhrase = hello + " " + world + "!";
+ console.log(epicPhrase); 
